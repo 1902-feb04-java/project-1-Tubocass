e7c5e92745e5ffdeeda35a2603c72d76652f3e9d
Brandon Lee <tubocass@gmail.com>
2019-02-21 13:43:12 -0600

    Create a sample obj from input

--- ERS/src/main/java/com/revature/App.java
+++ ERS/src/main/java/com/revature/App.java
@@ -1,13 +1,40 @@
 package com.revature;
 
-/**
- * Hello world!
- *
- */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        try(
+            InputStreamReader readIn = new InputStreamReader(System.in);
+            BufferedReader br = new BufferedReader(readIn);
+
+        ){
+            System.out.println("Enter your name and expensed amount");
+            String input = br.readLine();
+            Employee emp = new Employee(input);
+           
+        }catch(IOException exception){
+            exception.printStackTrace();
+        }
+
+    }
+    
+}
+class Employee{
+    String name, amount;
+    Employee(String line)
+    {
+        Scanner scan = new Scanner(line).useDelimiter(", ");
+
+        name = "name: "+scan.next();
+        amount = "amount: "+scan.next();
+        scan.close();
+        System.out.println(name +" - "+ amount);
     }
 }
+
