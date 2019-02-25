853da4f9d539957ef00fa291ed912e35fcdaecb7
Brandon Lee <tubocass@gmail.com>
2019-02-25 10:53:26 -0600

    Add Dao classes for reimbursement and employee.

---
+++ ERS/src/main/java/com/revature/DAO/EmployeeDAOImpl.java
@@ -0,0 +1,6 @@
+package com.revature.DAO;
+
+public class EmployeeDAOImpl implements EmployeeDAO
+{
+
+}
---
+++ ERS/src/main/java/com/revature/DAO/TestDAO.java
@@ -0,0 +1,22 @@
+package com.revature.DAO;
+
+import java.util.List;
+
+import com.revature.models.Reimbursement;
+
+public class TestDAO
+{
+	public static void main(String[] args)
+	{
+		ReimbursementDAO reDAO = DAOUtilities.getReimburseDAO();
+		
+		List<Reimbursement> requests = reDAO.getAllRequests();
+		
+		for(int i =0;i<requests.size(); i++)
+		{
+			System.out.println( requests.get(i).toString());
+		}
+	}
+}
+	
+
