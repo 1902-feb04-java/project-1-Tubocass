19039817550f77807b7704ad3fb31e09e6156a18
Brandon Lee <tubocass@gmail.com>
2019-02-26 19:36:13 -0600

    proto login page

--- Recompense/src/main/java/com/revature/servlets/HelloServlet.java
+++ Recompense/src/main/java/com/revature/servlets/HelloServlet.java
@@ -4,19 +4,33 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+@WebServlet("/hey")
 public class HelloServlet extends HttpServlet
 {
 	private static final long serialVersionUID = 1L;
 	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
 	{
-		PrintWriter pr = response.getWriter();
-		pr.println("<h1>Hey there "+request.getParameter("var")+"!</h1>");
-		pr.close();
+		HttpSession session = request.getSession();
+		String user = (String) session.getAttribute("user");
+		if(user == null)
+		{
+			user = request.getParameter("user");
+			session.setAttribute("user", user);
+		}
+		String password = request.getParameter("password");
+		
 //		response.sendRedirect("new.html");
+		
+		PrintWriter pr = response.getWriter();
+		pr.println("<h1>Hey there "+user+"!</h1>");
+		pr.close();
+		session.invalidate();
 	}
 }
